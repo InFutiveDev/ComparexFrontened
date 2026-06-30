ac4f001419901e1d5b11108c1cee01680c1f1e3a
vikash <93303641+InFutiveDev@users.noreply.github.com>
2026-06-30 18:05:19 +0530

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const DEFAULT_API_URL = "http://localhost:3000/api";
+const DEFAULT_API_URL = "http://100.54.237.0/api/";
 
 export const API_BASE_URL = (
   process.env.NEXT_PUBLIC_API_URL || DEFAULT_API_URL
